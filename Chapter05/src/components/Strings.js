bd6e82c99621cfad90a3b482571451a84e2937fa
Emilio <emiliorodriguez@gmail.com>
2019-06-27 19:02:46 +0200

    updated Chapter 5 to RN 0.59

--- Chapter05/src/components/Strings.js
+++ Chapter05/src/components/Strings.js
@@ -1,37 +1,39 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Image,
-  View,
-  Text
-} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Image, View, Text } from "react-native";
+import PropTypes from "prop-types";
+import { colors } from "../utils/";
 
-import { colors } from '../utils/';
-
-const stringNotes = ['E','A','D','G','B','E'];
+const stringNotes = ["E", "A", "D", "G", "B", "E"];
 
 export default class Strings extends Component {
   static propTypes = {
-    activeString: React.PropTypes.number
-  }
+    activeString: PropTypes.number
+  };
 
   render() {
     return (
       <View style={styles.stringsContainer}>
-        {
-          stringNotes.map((note, i) => {
-            return (
-              <View key={i} style={styles.stringContainer}>
-                <Image source={require('../../img/string.jpg')} style={styles.string}/>
-                <View style={[styles.noteContainer, {borderColor: (this.props.activeString === (i+1)) ? '#3bd78b' : '#f3c556'}]}>
-                  <Text style={styles.note}>
-                    {note}
-                  </Text>
-                </View>
+        {stringNotes.map((note, i) => {
+          return (
+            <View key={i} style={styles.stringContainer}>
+              <Image
+                source={require("../../img/string.jpg")}
+                style={styles.string}
+              />
+              <View
+                style={[
+                  styles.noteContainer,
+                  {
+                    borderColor:
+                      this.props.activeString === i + 1 ? "#3bd78b" : "#f3c556"
+                  }
+                ]}
+              >
+                <Text style={styles.note}>{note}</Text>
               </View>
-            )
-          })
-        }
+            </View>
+          );
+        })}
       </View>
     );
   }
@@ -41,22 +43,22 @@ const styles = StyleSheet.create({
   stringsContainer: {
     borderTopColor: colors.green,
     borderTopWidth: 5,
-    justifyContent: 'space-around',
-    flexDirection: 'row'
+    justifyContent: "space-around",
+    flexDirection: "row"
   },
   stringContainer: {
-    alignItems: 'center'
+    alignItems: "center"
   },
   note: {
-    color: 'white',
+    color: "white",
     fontSize: 19,
-    textAlign: 'center'
+    textAlign: "center"
   },
   noteContainer: {
     top: 50,
     height: 50,
     width: 50,
-    position: 'absolute',
+    position: "absolute",
     padding: 10,
     borderColor: colors.yellow,
     borderWidth: 3,
--- Chapter05/src/components/Tuner.js
+++ Chapter05/src/components/Tuner.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   Image,
@@ -6,29 +6,27 @@ import {
   Animated,
   Easing,
   Dimensions
-} from 'react-native';
+} from "react-native";
+import PropTypes from "prop-types";
 
-var {height, width} = Dimensions.get('window');
+var { height, width } = Dimensions.get("window");
 
 export default class Tuner extends Component {
   state = {
-    xIndicator:  new Animated.Value(width/2)
-  }
+    xIndicator: new Animated.Value(width / 2)
+  };
 
   static propTypes = {
-    delta: React.PropTypes.number
-  }
+    delta: PropTypes.number
+  };
 
   componentWillReceiveProps(newProps) {
-    if(this.props.delta !== newProps.delta) {
-      Animated.timing(
-        this.state.xIndicator,
-        {
-          toValue: (width/2) + (newProps.delta*width/2)/100,
-          duration: 500,
-          easing: Easing.elastic(2)
-        }
-      ).start();
+    if (this.props.delta !== newProps.delta) {
+      Animated.timing(this.state.xIndicator, {
+        toValue: width / 2 + (newProps.delta * width) / 2 / 100,
+        duration: 500,
+        easing: Easing.elastic(2)
+      }).start();
     }
   }
 
@@ -37,25 +35,28 @@ export default class Tuner extends Component {
 
     return (
       <View style={styles.tunerContainer}>
-        <Image source={require('../../img/tuner.jpg')} style={styles.tuner}/>
-        <Animated.Image source={require('../../img/indicator.jpg')} style={[styles.indicator, {left: xIndicator}]}/>
+        <Image source={require("../../img/tuner.jpg")} style={styles.tuner} />
+        <Animated.Image
+          source={require("../../img/indicator.jpg")}
+          style={[styles.indicator, { left: xIndicator }]}
+        />
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   tunerContainer: {
     flex: 1,
-    backgroundColor: '#1f2025',
+    backgroundColor: "#1f2025",
     marginTop: height * 0.05
   },
   tuner: {
     width,
-    resizeMode: 'contain'
+    resizeMode: "contain"
   },
   indicator: {
-    position: 'absolute',
+    position: "absolute",
     top: 10
   }
 });
